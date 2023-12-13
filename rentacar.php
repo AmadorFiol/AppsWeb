1c39d83246f40cf4c23c7a7363dd688275939a28
AmadorFiol <145798306+AmadorFiol@users.noreply.github.com>
2023-12-13 13:39:05 +0100

    rentacar.php

--- rentacar.php
+++ rentacar.php
@@ -19,23 +19,24 @@ $fulldata=$xml->rows;
 $i=0;
 $rellenar = array();
 foreach ($fulldata->row as $data) {
-    $municipio = (string) $data->municipi;
-    $adressa = $data->adre_a_de_l_establiment;
-    $postalcode = intval(preg_replace('/[^0-9]+/', '', $adressa), 10);
-    $nombre = $data->denominaci_comercial;
-    $cantidadCoches = (string) $data->nombre_de_vehicles;
+	$municipio = (string) $data->municipi;
+	$adressa = $data->adre_a_de_l_establiment;
+	preg_match('/\b\d{5}\b/', $adressa, $postalcode);
+	//$postalcode = intval(preg_replace('/[^0-9]+/', '', $adressa), 10);
+	$nombre = $data->denominaci_comercial;
+	$cantidadCoches = (string) $data->nombre_de_vehicles;
 
-    $establecimiento = array(
-        "nombre_comercial" => $nombre,
-        "codigo_postal" => $postalcode,
-        "cantidad_coches" => $cantidadCoches,
-        "direccion" => $adressa
-    );
-    if (isset($rellenar[$municipio])) {
-	    $rellenar[$municipio][] = $establecimiento;
-    } else {
-	    $rellenar[$municipio] = array($establecimiento);
-    }
+	$establecimiento = array(
+		"nombre_comercial" => $nombre,
+		"codigo_postal" => $postalcode,
+		"cantidad_coches" => $cantidadCoches,
+		"direccion" => $adressa
+	);
+	if (isset($rellenar[$municipio])) {
+		$rellenar[$municipio][] = $establecimiento;
+	} else {
+		$rellenar[$municipio] = array($establecimiento);
+	}
 }
 //var_dump($rellenar);
 echo "<br>";
