0f117f81efc57a2d57c8d482ce93d4709eb8e7be
AmadorFiol <145798306+AmadorFiol@users.noreply.github.com>
2023-12-17 11:11:33 +0100

    .

--- Actividades/php/rentacar.php
+++ Actividades/php/rentacar.php
@@ -48,46 +48,40 @@ ksort($rellenar)
 ?>
 
 <form method="post" action="rentacar.php">
-    <fieldset>
-        <legend>Selecciona un municipio:</legend>
-        <?php
-            foreach ($rellenar as $municipio => $establecimientos) {
-                echo "<input type=\"radio\" id=\"" . $municipio . "\" name=\"municipio\" value=\"" . $municipio . "\">";
-                echo "<label for=\"" . $municipio . "\">" . $municipio . "</label><br>";
-            }
-        ?>
-    </fieldset>
-  <?php
-// Crear un array para almacenar los códigos postales únicos
-$codigos_postales_unicos = array();
-
-// Recorrer los datos y agregar los códigos postales únicos al array
-foreach ($rellenar as $municipio => $establecimientos) {
-    foreach ($establecimientos as $establecimiento) {
-        foreach ($establecimiento['codigo_postal'] as $cp) {
-            // Agregar el código postal al array solo si no está presente
-            if (!in_array($cp, $codigos_postales_unicos)) {
-                $codigos_postales_unicos[] = $cp;
-            }
-        }
-    }
-}
-
-// Ordenar el array de códigos postales de forma numérica
-sort($codigos_postales_unicos, SORT_NUMERIC);
-
-// Imprimir las opciones del select box
-echo "<label for=\"codigo_postal\">Selecciona un código postal:</label>";
-echo "<select name=\"codigo_postal\" id=\"codigo_postal\">";
-echo "<option value=\"\">Selecciona un código postal</option>";
-foreach ($codigos_postales_unicos as $cp) {
-    echo "<option value=\"$cp\">$cp</option>";
-}
-echo "</select>";
-?>
-<label for="nombre">Introduce el nombre de la empresa:</label>
-<input type="text" name="nombre" id="nombre">
-    <input type="submit" value="Filtrar">
+	<fieldset>
+		<legend>Selecciona un municipio:</legend>
+		<?php
+			foreach ($rellenar as $municipio => $establecimientos) {
+				echo "<input type=\"radio\" id=\"" . $municipio . "\" name=\"municipio\" value=\"" . $municipio . "\">";
+				echo "<label for=\"" . $municipio . "\">" . $municipio . "</label><br>";
+			}
+		?>
+	</fieldset>
+	<?php
+		$codigos_postales_unicos = array();
+		foreach ($rellenar as $municipio => $establecimientos) {
+			foreach ($establecimientos as $establecimiento) {
+				foreach ($establecimiento['codigo_postal'] as $cp) {
+					if (!in_array($cp, $codigos_postales_unicos)) {
+						$codigos_postales_unicos[] = $cp;
+					}
+				}
+			}
+		}
+		sort($codigos_postales_unicos, SORT_NUMERIC);
+		echo "<label for=\"codigo_postal\">Selecciona un código postal:</label>";
+		echo "<select name=\"codigo_postal\" id=\"codigo_postal\">";
+		echo "<option value=\"\">Selecciona un código postal</option>";
+		foreach ($codigos_postales_unicos as $cp) {
+			echo "<option value=\"$cp\">$cp</option>";
+		}
+		echo "</select>";
+	?>
+	<br>
+	<label for="nombre">Introduce el nombre de la empresa:</label>
+	<input type="text" name="nombre" id="nombre">
+	<br>
+	<input type="submit" value="Filtrar">
 </form>
 <br>
 <?php
