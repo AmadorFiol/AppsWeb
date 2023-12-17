c34684eea194209e2ef5fbc016d4444144e69bfe
AmadorFiol <145798306+AmadorFiol@users.noreply.github.com>
2023-12-17 10:42:45 +0100

    Add files via upload

---
+++ Actividades/php/rentacar.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html>
+	<title>Ejercicio XML</title>
+
+<body>
+	<?php
+$url = "https://catalegdades.caib.cat/resource/rjfm-vxun.xml";
+if (!$xml = file_get_contents($url)) {
+	echo "No se ha podido cargar la url";
+} else {
+	//echo "Se ha podido cargar la url <br>";
+	$xml = simplexml_load_string($xml);
+	//var_dump($xml);
+}
+
+$fulldata=$xml->rows;
+//var_dump($fulldata);
+$i=0;
+$relleno=array();
+foreach($fulldata->row as $data) {
+	$municipio=(string) $data->municipi;
+	$adressa=$data->adre_a_de_l_establiment;
+	$postalcode = intval(preg_replace('/[^0-9]+/', '', $adressa), 10);
+	$nombre=$data->denominacio_comercial;
+	if (isset($rellenar[$municipio])) {
+        	   $rellenar[$municipio] = $postalcode;
+	    } else {
+	        $rellenar[$municipio] = $postalcode;
+	    }
+
+}
+var_dump($rellenar);
+echo "<br>";
+//var_dump($postalcode);
+echo "<br>";
+//var_dump($nombre);
+
+$municipio=isset($_GET["municipio"]) ? $_GET["municipio"] : "";
+$postalcode=isset($_GET["codigo_postal"]) ? $_GET["codigo_postal"] : "";
+$nombre=isset($_GET["nombre"]) ? $_GET["nombre"] : "";
+
+//ksort($municipio)
+?>
+
+<from action="rentacar.php" method="get">
+	<label for="municipios">Elije el municipio:</label>
+	<select id="municipios" name="municipios">
+		<?php foreach($data->municipi as $relleno[$municipio]){
+			echo "<option value=".$relleno[$municipio].">".$relleno[$municipio]."</option>";
+			}?>
+	</select>
+<?php
+//echo "Municipio: ".$municipio."<br>";
+//echo "Codigo postal: ".$postalcode."<br>";
+//echo "Nombre: ".$nombre."<br>";
+	?>
+
+</body>
+</html>
