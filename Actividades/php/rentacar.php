03e77a7aaaa12d561d25a47d707bb6320d320a3d
AmadorFiol <145798306+AmadorFiol@users.noreply.github.com>
2023-12-17 11:19:32 +0100

    .

--- Actividades/php/rentacar.php
+++ Actividades/php/rentacar.php
@@ -93,9 +93,7 @@ ksort($rellenar)
 		echo "<table border='1'>";
 		echo "<tr><th>Nombre Comercial</th><th>Cantidad de coches disponibles</th><th>Dirección</th></tr>";
 		foreach ($rellenar[$municipioSeleccionado] as $establecimiento) {
-			if ($nombreSeleccionado === "" || stripos($establecimiento['nombre_comercial'], $nombreSeleccionado) !== false) {
-				echo "<tr><td>" . $establecimiento['nombre_comercial'] . "</td><td>" . $establecimiento['cantidad_coches'] . "</td><td>" . $establecimiento['direccion'] . "</td></tr>";
-			}
+			echo "<tr><td>" . $establecimiento['nombre_comercial'] . "</td><td>" . $establecimiento['cantidad_coches'] . "</td><td>" . $establecimiento['direccion'] . "</td></tr>";
 		}
 		echo "</table>";
 	}
